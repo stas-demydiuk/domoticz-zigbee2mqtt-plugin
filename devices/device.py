779a2100f8bdf9e405b220daeb33d027359ae249
Stanislav Demydiuk <s.demydiuk@gmail.com>
2018-10-03 08:44:11 +0300

    Fix possible error with outdated devices from previous plugin versions

--- devices/device.py
+++ devices/device.py
@@ -1,5 +1,6 @@
 import Domoticz
 
+
 class Device():
     def __init__(self, devices, alias, value_key):
         self.devices = devices
@@ -13,7 +14,12 @@ class Device():
 
     def get_device(self, address, alias):
         for unit, device in self.devices.items():
-            if device.Options['address'] == address and device.Options['alias'] == alias:
+            if (
+                'address' in device.Options and
+                'alias' in device.Options and
+                device.Options['address'] == address and
+                device.Options['alias'] == alias
+            ):
                 return device
 
     def get_device_options(self, message):
@@ -24,7 +30,10 @@ class Device():
         }
 
     def _create_device(self, message):
-        Domoticz.Debug('Creating domoticz device to handle ' + self.value_key + ' key for device with ieeeAddr ' + message.get_device_ieee_addr())
+        Domoticz.Debug(
+            'Creating domoticz device to handle ' + self.value_key +
+            ' key for device with ieeeAddr ' + message.get_device_ieee_addr()
+        )
 
         device_name = message.get_device_name()
         unit = self.get_first_available_unit()
@@ -33,21 +42,30 @@ class Device():
         return self.create_device(unit, device_name, options, message)
 
     def create_device(self, unit, device_name, options, message):
-        Domoticz.Error('Unable to create device to handle ' + self.value_key +' value for device ' + message.get_device_ieee_addr())
+        Domoticz.Error(
+            'Unable to create device to handle ' + self.value_key +
+            ' value for device ' + message.get_device_ieee_addr()
+        )
 
     def get_numeric_value(self, value, device):
-        Domoticz.Error('Device with alias "' + self.alias + '" for key ' + self.value_key + ' can not calculate numeric value')
+        Domoticz.Error(
+            'Device with alias "' + self.alias + '" for key ' +
+            self.value_key + ' can not calculate numeric value'
+        )
 
     def get_string_value(self, value, device):
-        Domoticz.Error('Device with alias "' + self.alias + '" for key ' + self.value_key + ' can not calculate string value')
+        Domoticz.Error(
+            'Device with alias "' + self.alias + '" for key ' +
+            self.value_key + ' can not calculate string value'
+        )
 
     def handle_message(self, message):
-        device_address = message.get_device_ieee_addr()     
+        device_address = message.get_device_ieee_addr()
         device = self.get_device(device_address, self.alias)
 
         if (device == None):
             # Due to internal domoticz bug, app crashes if we try to use device just after we create it
-            # so just create and exit for now           
+            # so just create and exit for now
             # device = self._create_device(message)
             return self._create_device(message)
 
@@ -64,5 +82,10 @@ class Device():
 
         if (battery_level == None):
             battery_level = device.BatteryLevel
-            
-        device.Update(nValue=n_value, sValue=s_value, SignalLevel=signal_level, BatteryLevel=battery_level)
+
+        device.Update(
+            nValue=n_value,
+            sValue=s_value,
+            SignalLevel=signal_level,
+            BatteryLevel=battery_level
+        )
