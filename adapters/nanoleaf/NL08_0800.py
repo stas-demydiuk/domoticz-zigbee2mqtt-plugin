1c58b063b1e94075a43bea048356b221738ac914
Stanislav Demydiuk <s.demydiuk@gmail.com>
2018-10-31 08:52:03 +0200

    Use DimmerSwitch device for NL08_0800

--- adapters/nanoleaf/NL08_0800.py
+++ adapters/nanoleaf/NL08_0800.py
@@ -1,13 +1,19 @@
 import json
 from adapters.base_adapter import Adapter
-from devices.dim_light import DimLight
+from devices.switch.dimmer_switch import DimmerSwitch
 
 class NL08_0800(Adapter):
     """adapter for the Nanoleaf Smart Ivy bulbs"""
     def __init__(self, devices):
         super().__init__(devices)
-        self.devices.append(DimLight(devices, 'light', 'state_brightness'))
-        
+        self.devices.append(DimmerSwitch(devices, 'light', 'value'))
+
+    def convert_message(self, message):
+        if 'state' in message.raw and message.raw['state'].upper() == 'OFF':
+            message.raw['value'] = 0
+        elif 'brightness' in message.raw:
+            message.raw['value'] = message.raw['brightness']
+
     def handleCommand(self, alias, device, device_data, command, level, color):
         if command.upper() == 'ON' or command.upper() == 'OFF':
             return {
