f64371c0e3f678ee430980bb56cd55cb0e800f49
JanJaapKo <44156144+JanJaapKo@users.noreply.github.com>
2018-12-16 21:49:48 +0100

    Update rgb_adapter.py
    align to rgbw adapter

--- adapters/rgb_adapter.py
+++ adapters/rgb_adapter.py
@@ -36,11 +36,14 @@ class RGBAdapter(Adapter):
             green = colorObject['g']
             red = colorObject['r']
             blue = colorObject['b']
+            ttime = 1
 
-            return {
-                'topic': device_data['friendly_name'] + '/set',
-                'payload': json.dumps({
+            payload = json.dumps({
                     "state": "ON",
+              # Disabled the transition time for now because hue bulb/zigbee2mqtt will
+              # publish (acknowledge) the new color value during the transition with
+              # a value between start and end of the transition, not the actual target color
+              #      "transition" : ttime,
                     "brightness": int(level * 255 / 100),
                     "color": {
                         "r": red,
@@ -48,4 +51,8 @@ class RGBAdapter(Adapter):
                         "b": blue
                     }
                 })
+            Domoticz.Debug('Sending to ZigBee:' + str(payload))
+            return {
+                'topic': device_data['friendly_name'] + '/set',
+                'payload': payload
             }
