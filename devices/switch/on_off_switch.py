612e63af1eb4905c70d2eda6dc578ba8877a5d26
Stanislav Demydiuk <s.demydiuk@gmail.com>
2019-02-15 20:24:14 +0200

    #77 Supress update On/Off switch with the same state

--- devices/switch/on_off_switch.py
+++ devices/switch/on_off_switch.py
@@ -11,14 +11,18 @@ class OnOffSwitch(Device):
 
     def get_string_value(self, value, device):
         return 'On' if value.lower() == 'on' else 'Off'
-        
+
+    def update_device(self, device, values):
+        if values['nValue'] != device.nValue or values['sValue'] != device.sValue:
+            super().update_device(device, values)
+
     def handle_command(self, device_data, command, level, color):
         device_address = device_data['ieee_addr']
         device = self.get_device(device_address, self.alias)
 
         Domoticz.Debug('Command "' + command + '" from device "' + device.Name + '"')
 
-        device.Update(
-            nValue=self.get_numeric_value(command, device),
-            sValue=self.get_string_value(command, device)
-        )
+        self.update_device(device, {
+            'nValue': self.get_numeric_value(command, device),
+            'sValue': self.get_string_value(command, device)
+        })
