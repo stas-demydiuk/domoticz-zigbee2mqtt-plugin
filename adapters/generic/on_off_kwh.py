803c4dabbd9967d944efbc51f25fa106dbc406ae
Casper Meijn <casper@meijn.net>
2019-03-31 10:39:40 +0200

    Add support for NET2GRID N2G-SP
    This adds support for NET2GRID N2G-SP, which is an on/off switch
    with power and energy measurement.

--- adapters/generic/on_off_kwh.py
+++ adapters/generic/on_off_kwh.py
@@ -1,11 +1,9 @@
 from adapters.on_off_switch_adapter import OnOffSwitchAdapter
-from devices.sensor.voltage import VoltageSensor
 from devices.sensor.kwh import KwhSensor
 
 
-class Plug(OnOffSwitchAdapter):
+class OnOffKwhAdapter(OnOffSwitchAdapter):
     def __init__(self, devices):
         super().__init__(devices)
-        self.devices.append(VoltageSensor(devices, 'volt', 'voltage'))
-        values = ['power']
+        values = ['power', 'energy']
         self.devices.append(KwhSensor(devices, 'kwh', values))
--- devices/sensor/kwh.py
+++ devices/sensor/kwh.py
@@ -3,14 +3,30 @@ from devices.device import Device
 
 
 class KwhSensor(Device):
+    def __init__(self, devices, alias, value_key, device_name_suffix=''):
+        super().__init__(devices, alias, ';'.join(value_key), device_name_suffix)
+        self.value_keys = value_key
+    
     def create_device(self, unit, device_id, device_name):
         options = {}
         options['EnergyMeterMode'] = '1'
 
         return Domoticz.Device(Unit=unit, DeviceID=device_id, Name=device_name, TypeName="kWh", Options=options).Create()
 
+    def get_message_value(self, message):
+        value = {}
+
+        for item in self.value_keys:
+            if item in message.raw:
+                value[item] = message.raw[item]
+
+        return value if len(value) > 0 else None
+
     def get_numeric_value(self, value, device):
         return 0
 
     def get_string_value(self, value, device):
-        return str(value)
+        if 'energy' in value:
+            return str(value['power']) + ";" + str(value['energy'] * 1000)
+        else:
+            return str(value['power'])
