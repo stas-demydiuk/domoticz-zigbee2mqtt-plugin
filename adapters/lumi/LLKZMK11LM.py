99b5f7d3098a90ca9b7eca94e14e9fbea26494cd
bricamac <jcornaglia@gmail.com>
2019-08-10 00:43:43 +0200

    Add the support of XIAOMI Aqara LLKZMK11LM Two-way Control Module

---
+++ adapters/lumi/LLKZMK11LM.py
@@ -0,0 +1,24 @@
+import Domoticz
+import json
+from adapters.base_adapter import Adapter
+from devices.switch.on_off_switch import OnOffSwitch
+from devices.sensor.kwh import KwhSensor
+from devices.sensor.temperature import TemperatureSensor
+
+class LLKZMK11LM(Adapter):
+    def __init__(self, devices):
+        super().__init__(devices)
+
+        self.devices.append(OnOffSwitch(devices, 'l1', 'state_l1'))
+        self.devices.append(OnOffSwitch(devices, 'l2', 'state_l2'))
+        values = ['power', 'consumption']
+        self.devices.append(KwhSensor(devices, 'kwh', values))
+        self.devices.append(TemperatureSensor(devices, 'temp', 'temperature'))
+
+    def handleCommand(self, alias, device, device_data, command, level, color):
+        return {
+            'topic': '/'.join([device_data['friendly_name'], alias, 'set']),
+            'payload': json.dumps({
+                "state": command.upper()
+            })
+        }
