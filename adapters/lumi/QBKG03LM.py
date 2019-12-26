b0132264f2324c9ed8eb9021d08d5017919ec4d7
Stanislav Demydiuk <s.demydiuk@gmail.com>
2019-12-26 20:43:34 +0200

    Improve Xiaomi Aqara QBKG03LM support

--- adapters/lumi/QBKG03LM.py
+++ adapters/lumi/QBKG03LM.py
@@ -2,18 +2,33 @@ import Domoticz
 import json
 from adapters.base_adapter import Adapter
 from devices.switch.on_off_switch import OnOffSwitch
+from devices.switch.selector_switch import SelectorSwitch
 
 class QBKG03LM(Adapter):
     def __init__(self, devices):
         super().__init__(devices)
         
+        selector = SelectorSwitch(devices, 'click', 'click')
+        selector.add_level('Off', None)
+        selector.add_level('Left', 'left')
+        selector.add_level('Right', 'right')
+        selector.add_level('Both', 'both')
+        selector.disable_value_check_on_update()
+
         self.devices.append(OnOffSwitch(devices, 'left', 'state_left'))
         self.devices.append(OnOffSwitch(devices, 'right', 'state_right'))
+        self.devices.append(selector)
 
     def handleCommand(self, alias, device, device_data, command, level, color):
-        return {
-            'topic': '/'.join([device_data['friendly_name'], alias, 'set']),
-            'payload': json.dumps({
-                "state": command.upper()
-            })
-        }
+        if alias == 'left' or alias == 'right':
+            return {
+                'topic': '/'.join([device_data['friendly_name'], alias, 'set']),
+                'payload': json.dumps({
+                    "state": command.upper()
+                })
+            }
+        else:
+            device = self.get_device_by_alias(alias)
+
+            if device != None:
+                device.handle_command(device_data, command, level, color)
