cd6f8b86d96c5df4ab75e8f003b431c6fbb72b29
Stanislav Demydiuk <s.demydiuk@gmail.com>
2020-04-13 21:32:11 +0300

    #290 Display list of available devices

--- api/commands.py
+++ api/commands.py
@@ -1,5 +1,8 @@
 from api.network_map import NetworkMap
+from api.get_devices import GetDevices
+
 
 commands = dict({
-    'network_map': NetworkMap
+    'network_map': NetworkMap,
+    'devices_get': GetDevices
 })
