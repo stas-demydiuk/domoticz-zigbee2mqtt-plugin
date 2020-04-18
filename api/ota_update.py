e01d1b7da2c49842dd42c300ecb110509b1e9681
janjaapko <janjaap.kostelijk@gmail.com>
2020-04-18 14:56:42 +0200

    touchup modal form

--- api/ota_update.py
+++ api/ota_update.py
@@ -5,5 +5,10 @@ from api.command import APICommand
 
 class OtaUpdate(APICommand):
     def execute(self, params):
+        Domoticz.Debug("API OTACommand for: params = '" + str(params) + "' self.request_id = '" + str(self.request_id) + "'")
         self.publish_mqtt('bridge/ota_update/update', params['state'])
-        self.send_response('ok')
+        #self.send_response('ok')
+        
+    def handle_mqtt_message(self, topic, message):
+        if topic == 'bridge/log' and message['type'] == 'ota_update':
+            self.send_response(message['message'])
