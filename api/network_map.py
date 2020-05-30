42e572a844e27511d0a620cf4cacfc768a5ae5f4
janjaapko <janjaap.kostelijk@gmail.com>
2020-05-30 21:04:46 +0200

    add routes to NW map

--- api/network_map.py
+++ api/network_map.py
@@ -3,7 +3,7 @@ from api.command import APICommand
 
 class NetworkMap(APICommand):
     def execute(self, params):
-        self.publish_mqtt('bridge/networkmap', 'graphviz')
+        self.publish_mqtt('bridge/networkmap/routes', 'graphviz')
 
     def handle_mqtt_message(self, topic, message):
         if topic == 'bridge/networkmap/graphviz':
