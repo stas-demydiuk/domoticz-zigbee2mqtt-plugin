7347b0d3874f1d3bfb1418ae91e75afc6d3d0179
Stanislav Demydiuk <s.demydiuk@gmail.com>
2020-06-28 11:47:12 +0300

    #108 Convert color_temp to 255 scale for RGB and RGBW devices

--- adapters/rgbw_adapter.py
+++ adapters/rgbw_adapter.py
@@ -11,6 +11,14 @@ class RGBWAdapter(Adapter, RGBMixin):
         self.dimmer = RGBWLight(devices, 'light', 'state_brightness_color')
         self.devices.append(self.dimmer)
 
+    def convert_message(self, message):
+        message = super().convert_message(message)
+
+        if 'color_temp' in message.raw:
+            message.raw['color_temp'] = int(message.raw['color_temp'] * 255 / 500)
+
+        return message
+
     def handleCommand(self, alias, device, device_data, command, level, color):
         topic = device_data['friendly_name'] + '/set'
         return self.set_color(topic, command, level, color)
