a58a9fb3136e93d81149d363419b04cd6688d46e
Kamil Bemowski <kamil@bemowski.pl>
2020-07-03 12:42:01 +0200

    add support for WXKG07LM

--- adapters/lumi/WXKG07LM.py
+++ adapters/lumi/WXKG07LM.py
@@ -6,15 +6,22 @@ class WXKG07LM(AdapterWithBattery):
     def __init__(self, devices):
         super().__init__(devices)
 
-        self.switch = SelectorSwitch(devices, 'switch', 'click')
-        self.switch.add_level('Off', None)
-        self.switch.add_level('Switch 1', 'left')
-        self.switch.add_level('Switch 2', 'right')
-        self.switch.add_level('Both_Click', 'both')
-        self.switch.set_selector_style(SelectorSwitch.SELECTOR_TYPE_BUTTONS)
-        self.switch.disable_value_check_on_update()
+        self.switchLeft = SelectorSwitch(devices, 'switch_left', 'click')
+        self.switchLeft.add_level('Switch 1', 'left')
+        self.switchLeft.add_level('Switch 1', 'left_long')
+        self.switchLeft.add_level('Switch 1', 'left_double')
+        self.switchLeft.set_selector_style(SelectorSwitch.SELECTOR_TYPE_BUTTONS)
+        self.switchLeft.disable_value_check_on_update()
+        self.switchRight = SelectorSwitch(devices, 'switch_right', 'click')
+        self.switchRight.add_level('Switch 2', 'right')
+        self.switchRight.add_level('Switch 2', 'right_long')
+        self.switchRight.add_level('Switch 2', 'right_double')
+        self.switchBoth = SelectorSwitch(devices, 'both_click', 'click')
+        self.switchBoth.add_level('Both Click', 'both')
 
-        self.devices.append(self.switch)
+        self.devices.append(self.switchLeft)
+        self.devices.append(self.switchRight)
+        self.devices.append(self.switchBoth)
 
     def handleCommand(self, alias, device, device_data, command, level, color):
-        self.switch.handle_command(device_data, command, level, color)
+        self.switchLeft.handle_command(device_data, command, level, color)
