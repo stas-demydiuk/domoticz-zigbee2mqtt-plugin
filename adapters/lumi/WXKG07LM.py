7facb3612c60feb6a1a1745b91d2e3a66cdc53de
Kamil Bemowski <kamil@bemowski.pl>
2020-07-03 10:59:58 +0200

    add support for WXKG07LM

---
+++ adapters/lumi/WXKG07LM.py
@@ -0,0 +1,20 @@
+from adapters.adapter_with_battery import AdapterWithBattery
+from devices.switch.selector_switch import SelectorSwitch
+
+
+class WXKG07LM(AdapterWithBattery):
+    def __init__(self, devices):
+        super().__init__(devices)
+
+        self.switch = SelectorSwitch(devices, 'switch', 'click')
+        self.switch.add_level('Off', None)
+        self.switch.add_level('Switch 1', 'left')
+        self.switch.add_level('Switch 2', 'right')
+        self.switch.add_level('Both_Click', 'both')
+        self.switch.set_selector_style(SelectorSwitch.SELECTOR_TYPE_BUTTONS)
+        self.switch.disable_value_check_on_update()
+
+        self.devices.append(self.switch)
+
+    def handleCommand(self, alias, device, device_data, command, level, color):
+        self.switch.handle_command(device_data, command, level, color)
