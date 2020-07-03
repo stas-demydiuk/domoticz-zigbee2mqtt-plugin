76aee7b7eb13854d82ab1d5fae27fc0add1ee633
Kamil Bemowski <kamil@bemowski.pl>
2020-07-03 12:53:29 +0200

    add support for WXKG07LM

--- adapters/lumi/WXKG07LM.py
+++ adapters/lumi/WXKG07LM.py
@@ -6,17 +6,17 @@ class WXKG07LM(AdapterWithBattery):
     def __init__(self, devices):
         super().__init__(devices)
 
-        self.switchLeft = SelectorSwitch(devices, 'switch_left', 'click')
+        self.switchLeft = SelectorSwitch(devices, 'switch', 'click')
         self.switchLeft.add_level('Switch 1', 'left')
         self.switchLeft.add_level('Switch 1', 'left_long')
         self.switchLeft.add_level('Switch 1', 'left_double')
         self.switchLeft.set_selector_style(SelectorSwitch.SELECTOR_TYPE_BUTTONS)
         self.switchLeft.disable_value_check_on_update()
-        self.switchRight = SelectorSwitch(devices, 'switch_right', 'click')
+        self.switchRight = SelectorSwitch(devices, 'switch', 'click')
         self.switchRight.add_level('Switch 2', 'right')
         self.switchRight.add_level('Switch 2', 'right_long')
         self.switchRight.add_level('Switch 2', 'right_double')
-        self.switchBoth = SelectorSwitch(devices, 'both_click', 'click')
+        self.switchBoth = SelectorSwitch(devices, 'switch', 'click')
         self.switchBoth.add_level('Both Click', 'both')
 
         self.devices.append(self.switchLeft)
