6b5d1a24e6fbee962009a0ddc86ecb80e5850086
jasonlim97 <68777296+jasonlim97@users.noreply.github.com>
2020-07-25 21:13:47 +0800

    Added 2 gangs without neutral wire switch

--- adapters/tuyatec/__init__.py
+++ adapters/tuyatec/__init__.py
@@ -2,6 +2,7 @@ from adapters.on_off_switch_adapter import OnOffSwitchAdapter
 from adapters.dimmable_bulb_adapter import DimmableBulbAdapter
 from adapters.generic.motion_sensor import MotionSensorAdapter
 from adapters.generic.temp_hum_sensor import TemperatureHumiditySensorAdapter
+from adapters.tuyatec.gdkes02tzxd import gdkes02tzxd
 
 
 tuyatec_adapters = {
@@ -9,4 +10,5 @@ tuyatec_adapters = {
     'TT001ZAV20': TemperatureHumiditySensorAdapter, # TUYATEC Temperature & humidity sensor
     'GDKES-01TZXD': OnOffSwitchAdapter,             # TUYATEC Smart light switch - 1 gang without neutral wire
     'gq8b1uv': DimmableBulbAdapter,                 # TUYATEC Zigbee smart dimmer
+    'GDKES-02TZXD': gdkes02tzxd,                    # TUYATEC Smart light switch - 2 gangs without neutral
 }
