1dacd61ae73e63efdb47e305a8caf694c30b5905
jasonlim97 <68777296+jasonlim97@users.noreply.github.com>
2020-07-25 21:30:01 +0800

    Update __init__.py
    add tuyatec 3 gangs supports

--- adapters/tuyatec/__init__.py
+++ adapters/tuyatec/__init__.py
@@ -3,12 +3,13 @@ from adapters.dimmable_bulb_adapter import DimmableBulbAdapter
 from adapters.generic.motion_sensor import MotionSensorAdapter
 from adapters.generic.temp_hum_sensor import TemperatureHumiditySensorAdapter
 from adapters.tuyatec.gdkes02tzxd import gdkes02tzxd
-
+from adapters.tuyatec.GDKES03TZXD import GDKES03TZXD
 
 tuyatec_adapters = {
     'RH3040': MotionSensorAdapter,                  # TUYATEC PIR sensor
     'TT001ZAV20': TemperatureHumiditySensorAdapter, # TUYATEC Temperature & humidity sensor
     'GDKES-01TZXD': OnOffSwitchAdapter,             # TUYATEC Smart light switch - 1 gang without neutral wire
     'gq8b1uv': DimmableBulbAdapter,                 # TUYATEC Zigbee smart dimmer
-    'GDKES-02TZXD': gdkes02tzxd,                    # TUYATEC Smart light switch - 2 gangs without neutral
+    'GDKES-02TZXD': gdkes02tzxd,                   # TUYATEC Smart light switch - 2 gangs without neutral
+    'GDKES-03TZXD': GDKES03TZXD,                    # TUYATEC Smart light switch - 3 gangs without neutral
 }
