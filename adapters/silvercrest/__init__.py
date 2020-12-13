04b7d3f505089bb02b45920176e8becc69b88547
Tech49 <plando2act@users.noreply.github.com>
2020-12-13 23:52:09 +0100

    Update __init__.py
    Trying if the same applies to the Z2M reported model code for the RGB light

--- adapters/silvercrest/__init__.py
+++ adapters/silvercrest/__init__.py
@@ -14,5 +14,6 @@ silvercrest_adapters = {
     'TY0203': ContactAdapter,           # Silvercrest Smart Window or Door sensor
     'HG06336':HG06336,                  # Silvercrest Smart Window or Door sensor
     'HG06337':OnOffSwitchAdapter,       # Silvercrest Smart Home Socket sold by LIDL is labelled HG6337 by Z2M
+    'TS0505A':RGBWAdapter,              # Silvercrest RGB Light Sold By Lidl [ not tested yet ] 
     'TY0111': OnOffSwitchAdapter,       # Silvercrest Smart Home Socket
 }
