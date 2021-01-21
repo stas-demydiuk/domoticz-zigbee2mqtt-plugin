98bca0aa470a9dd708b18e9d8b8359f78f1c89ef
Stanislav Demydiuk <s.demydiuk@gmail.com>
2021-01-21 20:16:12 +0200

    #547 Add `power_outage_memory`, `auto_off``, `away_mode`  and `led_disabled_night` features support

--- adapter.py
+++ adapter.py
@@ -178,25 +178,25 @@ class UniversalAdapter(Adapter):
             self._add_device(alias, feature, OnOffSwitch)
             return
 
-        # if (feature['name'] == 'led_disabled_night' and state_access and write_access):
-        #     alias = feature['endpoint'] if 'endpoint' in feature else 'nled'
-        #     self._add_device(alias, feature, OnOffSwitch)
-        #     return
+        if (feature['name'] == 'led_disabled_night' and state_access and write_access):
+            alias = feature['endpoint'] if 'endpoint' in feature else 'nled'
+            self._add_device(alias, feature, OnOffSwitch)
+            return
 
-        # if (feature['name'] == 'power_outage_memory' and state_access and write_access):
-        #     alias = feature['endpoint'] if 'endpoint' in feature else 'pwrmem'
-        #     self._add_device(alias, feature, OnOffSwitch, ' (Power Outage Memory)')
-        #     return
+        if (feature['name'] == 'power_outage_memory' and state_access and write_access):
+            alias = feature['endpoint'] if 'endpoint' in feature else 'pwrmem'
+            self._add_device(alias, feature, OnOffSwitch, ' (Power Outage Memory)')
+            return
 
-        # if (feature['name'] == 'auto_off' and state_access and write_access):
-        #     alias = feature['endpoint'] if 'endpoint' in feature else 'autoff'
-        #     self._add_device(alias, feature, OnOffSwitch, ' (Auto Off)')
-        #     return
+        if (feature['name'] == 'auto_off' and state_access and write_access):
+            alias = feature['endpoint'] if 'endpoint' in feature else 'autoff'
+            self._add_device(alias, feature, OnOffSwitch, ' (Auto Off)')
+            return
 
-        # if (feature['name'] == 'away_mode' and state_access and write_access):
-        #     alias = feature['endpoint'] if 'endpoint' in feature else 'away'
-        #     self._add_device(alias, feature, OnOffSwitch)
-        #     return
+        if (feature['name'] == 'away_mode' and state_access and write_access):
+            alias = feature['endpoint'] if 'endpoint' in feature else 'away'
+            self._add_device(alias, feature, OnOffSwitch)
+            return
 
         domoticz.error(self.name + ': can not process binary item "' + feature['name'] + '"')
         domoticz.debug(json.dumps(feature))
