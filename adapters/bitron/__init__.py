c3488ea7b267da77854d58a04792b231cf55453c
Flying Domotic <71657879+FlyingDomotic@users.noreply.github.com>
2021-04-15 12:20:38 +0200

    Add support of Bitron 902010/29 siren
    Add BitronSirenAdapterWithBattery class to support 902010/29 outdoor siren

--- adapters/bitron/__init__.py
+++ adapters/bitron/__init__.py
@@ -1,6 +1,7 @@
 from adapters.bitron.AV201032 import AV201032
-
+from adapters.bitron.siren import BitronSirenAdapterWithBattery
 
 bitron_adapters = {
-    'AV2010/32': AV201032,              # Bitron Wireless wall thermostat with relay
+    'AV2010/32': AV201032,                     # Bitron Wireless wall thermostat with relay
+    '902010/29': BitronSirenAdapterWithBattery # Bitron Outdoor siren
 }
