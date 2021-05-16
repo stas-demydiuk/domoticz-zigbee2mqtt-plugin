2f34bddd3d3863b462f6e081e3ed2769ae2b809d
bartrikers6224 <83872170+bartrikers6224@users.noreply.github.com>
2021-05-16 09:40:18 +0200

    Update __init__.py

--- adapters/ecodim/__init__.py
+++ adapters/ecodim/__init__.py
@@ -1,7 +1,7 @@
 from adapters.base_adapter import Adapter
-from adapters.ecodim.zigbee2buttonwallswitch import Zigbee2ButtonWallSwitch
+from adapters.ecodim.zigbee2buttonwallswitchBlack import Zigbee2ButtonWallSwitchBlack
 
 
 ecodim_adapters = {
-    'ED-10011': Zigbee2ButtonWallSwitch,  # EcoDim Zigbee 2 button wall switch - black
+    'ED-10011': Zigbee2ButtonWallSwitchBlack,  # EcoDim Zigbee 2 button wall switch - black
 }
