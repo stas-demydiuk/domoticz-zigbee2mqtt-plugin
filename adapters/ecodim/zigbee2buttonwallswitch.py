a2ac89426ec8f979ac17b67b891d30cab20748b4
bartrikers6224 <83872170+bartrikers6224@users.noreply.github.com>
2021-05-16 09:27:40 +0200

    Create zigbee2buttonwallswitch.py

---
+++ adapters/ecodim/zigbee2buttonwallswitch.py
@@ -0,0 +1,19 @@
+from adapters.adapter_with_battery import AdapterWithBattery
+from devices.switch.dimmer_switch import DimmerSwitch
+from devices.switch.selector_switch import SelectorSwitch
+
+class Zigbee2ButtonWallSwitch(AdapterWithBattery):
+    def __init__(self, devices):
+        super().__init__(devices)
+        self.devices.append(DimmerSwitch(devices, 'dimmer', 'brightness'))
+
+        self.switch = SelectorSwitch(devices, 'switch', 'action')
+        self.switch.add_level('Off', 'off')
+        self.switch.add_level('On', 'on')
+        self.switch.add_level('Up', 'brightness_move_up')
+        self.switch.add_level('Down', 'brightness_move_down')
+        self.switch.add_level('Stop', 'brightness_stop')
+        self.switch.set_selector_style(SelectorSwitch.SELECTOR_TYPE_MENU)
+        self.switch.disable_value_check_on_update()
+
+        self.devices.append(self.switch)
