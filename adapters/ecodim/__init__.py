b006f6d52e26dde2c4efdbd8aaf8504faffd3352
bartrikers6224 <83872170+bartrikers6224@users.noreply.github.com>
2021-06-09 19:21:24 +0200

    EcoDim ED-10011 support (#643)

---
+++ adapters/ecodim/__init__.py
@@ -0,0 +1,7 @@
+from adapters.base_adapter import Adapter
+from adapters.ecodim.zigbee2buttonwallswitchblack import Zigbee2ButtonWallSwitchBlack
+
+
+ecodim_adapters = {
+    'ED-10011': Zigbee2ButtonWallSwitchBlack,  # EcoDim Zigbee 2 button wall switch - black
+}
