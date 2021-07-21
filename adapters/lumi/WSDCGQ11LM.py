87002611d685b509399c3e3e80c81124225d68da
rolfie23 <61058985+rolfie23@users.noreply.github.com>
2021-07-21 18:07:53 +0200

    Create WSDCGQ11LM.py
    Support combined sensor Xiaomi Aqara

---
+++ adapters/lumi/WSDCGQ11LM.py
@@ -0,0 +1,31 @@
+from adapters.adapter_with_battery import AdapterWithBattery
+from devices.sensor.temperature import TemperatureSensor
+from devices.sensor.humidity import HumiditySensor
+from devices.sensor.pressure import PressureSensor
+from devices.sensor.barometer import BarometerSensor
+from devices.temperature_humidity_sensor import TemperatureHumiditySensor
+from devices.temperature_humidity_barometer_sensor import TemperatureHumidityBarometerSensor
+
+
+# Xiaomi Aqara temperature, humidity and pressure sensor 
+class WSDCGQ11LM(AdapterWithBattery):
+    def __init__(self, devices):
+        super().__init__(devices)
+        self.devices.append(TemperatureSensor(devices, 'temp', 'temperature', ' (Temperature)'))
+        self.devices.append(HumiditySensor(devices, 'hum', 'humidity', ' (Humidity)'))
+        self.devices.append(PressureSensor(devices, 'pres', 'pressure', ' (Pressure)'))
+        self.devices.append(BarometerSensor(devices, 'bar', 'pressure', ' (Barometer)'))
+        self.devices.append(TemperatureHumiditySensor(devices, 'th', 'combined', ' (Temperature + Humidity)'))
+        self.devices.append(TemperatureHumidityBarometerSensor(devices, 'all', 'combined', ' (Temperature + Humidity + Barometer)'))
+
+    def convert_message(self, message):
+        message = super().convert_message(message)
+
+        if 'temperature' in message.raw and 'humidity' in message.raw and 'pressure' in message.raw:
+            message.raw['combined'] = {
+                'temperature': message.raw['temperature'],
+                'humidity': message.raw['humidity'],
+                'pressure': message.raw['pressure']
+            }
+
+        return message
