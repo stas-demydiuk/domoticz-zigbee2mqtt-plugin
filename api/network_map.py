3ff6af3848163af5cca2598c18c7c3dc29747c36
Stanislav Demydiuk <s.demydiuk@gmail.com>
2021-12-25 12:12:57 +0200

    #735 Migrate from legacy network_map api

--- api/network_map.py
+++ api/network_map.py
@@ -3,8 +3,13 @@ from api.command import APICommand
 
 class NetworkMap(APICommand):
     def execute(self, params):
-        self.publish_mqtt('bridge/networkmap/routes', 'graphviz')
+        self.publish_mqtt('bridge/request/networkmap', 'graphviz')
 
     def handle_mqtt_message(self, topic, message):
-        if topic == 'bridge/networkmap/graphviz':
-            self.send_response(message)
+        if topic == 'bridge/response/networkmap':
+            if message['status'] == 'ok':
+                self.send_response(message['data']['value'])
+            elif message['status'] == 'error':
+                self.send_error(message['error'])
+            else:
+                self.send_error('Unable to get zigbee network map')
