8a9b9e3545b1cf44e9b5b1d54d58b6876b25e1cd
Stanislav Demydiuk <s.demydiuk@gmail.com>
2021-12-26 16:17:50 +0200

    Fix voltage value in mV

--- devices/sensor/voltage.py
+++ devices/sensor/voltage.py
@@ -6,8 +6,19 @@ class VoltageSensor(Device):
     def create_device(self, unit, device_id, device_name):
         return domoticz.create_device(Unit=unit, DeviceID=device_id, Name=device_name, TypeName="Voltage")
 
+    def get_voltage_value(self, value):
+        if hasattr(self, 'feature'):
+            unit = self.feature['unit']
+
+            if (unit == 'mV'):
+                return value / 1000
+            else:
+                return value
+        else:
+            return value
+
     def get_numeric_value(self, value, device):
-        return int(value)
+        return int(self.get_voltage_value(value))
 
     def get_string_value(self, value, device):
-        return str(value)
+        return str(self.get_voltage_value(value))
